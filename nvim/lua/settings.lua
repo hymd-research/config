67320b99bb76fc851890c5dae277bdb078bab230
hymd-research <hymd@hymd-devenv.local>
2022-06-05 15:50:15 +0900

    fix(nvim): do not use <C-r> for NvimTreeToggle

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,6 +1,6 @@
 vim.wo.number = true
 vim.cmd "colorscheme nord"
+vim.cmd "set termguicolors"
+
 vim.opt_global.completeopt = { "menu", "menuone", "noselect" }
 
-vim.keymap.set('n', '<C-r>', ':NvimTreeToggle<CR>')
-vim.cmd "set termguicolors"
