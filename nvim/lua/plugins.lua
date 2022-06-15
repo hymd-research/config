292293fa2990ba4937a09b53fe9fd6df7d08cd70
hymd <hymd.email@gmail.com>
2022-06-16 02:06:42 +0900

    fix(nvim): fix fzf-lua settings.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,60 +1,70 @@
 require'jetpack'.startup(function (use)
-	use { 'tani/vim-jetpack', opt = true }
-	use { 'kyazdani42/nvim-web-devicons' }
-	use { 'nvim-lua/plenary.nvim' }
-	use { 'mfussenegger/nvim-dap' }
-	use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' } }
-	use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', } }
-	use { 'neovim/nvim-lspconfig' }
-	use { 'williamboman/nvim-lsp-installer' }
-	use { 'L3MON4D3/LuaSnip' }
-	use { 'saadparwaiz1/cmp_luasnip' }
-	use { 'hrsh7th/nvim-cmp' }
-	use { 'hrsh7th/cmp-nvim-lsp' }
-	use { 'hrsh7th/cmp-buffer' }
-	use { 'folke/lua-dev.nvim' }
-	use { 'dag/vim-fish' }
-	use { 'hashivim/vim-terraform' }
-	use { 'simrat39/rust-tools.nvim' }
+	use {
+		'tani/vim-jetpack',
+		opt = true
+	}
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = { ':TSUpdate' }
+	}
+	use {
+		'kyazdani42/nvim-web-devicons'
+	}
+	use {
+		'nvim-lua/plenary.nvim'
+	}
+	use {
+		'mfussenegger/nvim-dap'
+	}
+	use {
+		'TimUntersberger/neogit',
+		requires = { 'nvim-lua/plenary.nvim' }
+	}
+	use {
+		'sindrets/diffview.nvim'
+	}
+	use {
+		'ibhagwan/fzf-lua'
+	}
+	use {
+		'kyazdani42/nvim-tree.lua',
+		requires = { 'kyazdani42/nvim-web-devicons' }
+	}
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'kyazdani42/nvim-web-devicons', }
+	}
+	use {
+		'neovim/nvim-lspconfig'
+	}
+	use {
+		'williamboman/nvim-lsp-installer'
+	}
+	use {
+		'L3MON4D3/LuaSnip'
+	}
+	use {
+		'saadparwaiz1/cmp_luasnip'
+	}
+	use {
+		'hrsh7th/nvim-cmp'
+	}
+	use {
+		'hrsh7th/cmp-nvim-lsp'
+	}
+	use {
+		'hrsh7th/cmp-buffer'
+	}
+	use {
+		'folke/lua-dev.nvim'
+	}
+	use {
+		'dag/vim-fish'
+	}
+	use {
+		'hashivim/vim-terraform'
+	}
+	use {
+		'simrat39/rust-tools.nvim'
+	}
 end)
-
-require'nvim-web-devicons'.setup {
-	default = true
-}
-
-require'nvim-tree'.setup {
-}
-
-require'rust-tools'.setup{
-}
-
-require'lualine'.setup {
-	options = {
-		icons_enabled = true,
-		theme = 'nord',
-		component_separators = { left = '', right = ''},
-		section_separators = { left = '', right = ''},
-		disabled_filetypes = {},
-		always_divide_middle = true,
-		globalstatus = false,
-	},
-	sections = {
-		lualine_a = {'mode'},
-		lualine_b = {'branch', 'diff', 'diagnostics'},
-		lualine_c = {'filename'},
-		lualine_x = {'encoding', 'fileformat', 'filetype'},
-		lualine_y = {'progress'},
-		lualine_z = {'location'}
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = {'filename'},
-		lualine_x = {'location'},
-		lualine_y = {},
-		lualine_z = {}
-	},
-	tabline = {},
-	extensions = {}
-}
-
--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,6 +1,168 @@
-vim.wo.number = true
-vim.cmd "colorscheme nord"
-vim.cmd "set termguicolors"
+vim.cmd "colorscheme iceberg"
+
+vim.opt.tabstop = 4
+vim.opt.number = true
+vim.opt.autoindent = true
+vim.opt.splitright = true
+vim.opt.termguicolors = true
+vim.opt.incsearch = true
+vim.opt.smartcase = true
 
 vim.opt_global.completeopt = { "menu", "menuone", "noselect" }
+vim.opt_global.diffopt = { 'internal', 'filler', 'closeoff', 'vertical' }
+
+require'nvim-web-devicons'.setup {
+	default = true,
+}
+
+require'nvim-tree'.setup {
+}
+
+require'nvim-treesitter.configs'.setup {
+	highlight = { enable = true },
+	indent = { enable = true }
+}
+
+require'fzf-lua'.setup {
+	keymap = {
+		fzf = {
+			["ctrl-d"] = 'kill-line',
+		}
+	},
+	fzf_bin = 'sk',
+	files = {
+		prompt       = 'Files❯ ',
+		multiprocess = true,
+		cmd          = 'fd --follow --hidden --color always --threads 4 --type f --type l --exclude .git',
+	},
+	grep = {
+		prompt       = 'Rg❯ ',
+		multiprocess = true,
+		cmd          = 'rg --line-number --no-heading --smart-case --color always',
+	},
+}
+
+require'lualine'.setup {
+	options = {
+		icons_enabled = true,
+		theme = 'iceberg',
+		component_separators = { left = '', right = ''},
+		section_separators = { left = '', right = ''},
+		disabled_filetypes = {},
+		always_divide_middle = true,
+		globalstatus = false,
+	},
+	sections = {
+		lualine_a = {
+			{ 'mode' }
+		},
+		lualine_b = {
+			{ 'branch' },
+			{ 'diff', symbols = { added = '+', modified = '~', removed = '-' } },
+			{ 'diagnostics' }
+		},
+		lualine_c = {
+			{ 'filename', path = 1 }
+		},
+		lualine_x = {
+			{ 'encoding' },
+			{ 'fileformat' },
+			{ 'filetype' }
+		},
+		lualine_y = {
+			{ 'progress' }
+		},
+		lualine_z = {
+			{ 'location' }
+		}
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = {
+			{ 'filename', path = 1 }
+		},
+		lualine_x = {'location'},
+		lualine_y = {},
+		lualine_z = {}
+	},
+	tabline = {},
+	extensions = {}
+}
+
+require("neogit").setup {
+	disable_signs = false,
+	disable_hint = false,
+	disable_context_highlighting = false,
+	disable_commit_confirmation = false,
+	-- Neogit refreshes its internal state after specific events, which can be expensive depending on the repository size.
+	-- Disabling `auto_refresh` will make it so you have to manually refresh the status after you open it.
+	auto_refresh = true,
+	disable_builtin_notifications = false,
+	use_magit_keybindings = false,
+	commit_popup = {
+		kind = "split",
+	},
+	-- Change the default way of opening neogit
+	kind = "tab",
+	-- customize displayed signs
+	signs = {
+		-- { CLOSED, OPENED }
+		section = { ">", "v" },
+		item = { ">", "v" },
+		hunk = { "", "" },
+	},
+	integrations = {
+		-- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `sindrets/diffview.nvim`.
+		-- The diffview integration enables the diff popup, which is a wrapper around `sindrets/diffview.nvim`.
+		--
+		-- Requires you to have `sindrets/diffview.nvim` installed.
+		-- use {
+		-- 'TimUntersberger/neogit',
+		-- requires = {
+		-- 'nvim-lua/plenary.nvim',
+		-- 'sindrets/diffview.nvim'
+		-- }
+		-- }
+		--
+		diffview = true
+	},
+	-- Setting any section to `false` will make the section not render at all
+	sections = {
+		untracked = {
+			folded = false
+		},
+		unstaged = {
+			folded = false
+		},
+		staged = {
+			folded = false
+		},
+		stashes = {
+			folded = true
+		},
+		unpulled = {
+			folded = true
+		},
+		unmerged = {
+			folded = false
+		},
+		recent = {
+			folded = true
+		},
+	},
+	-- override/add mappings
+	mappings = {
+		-- modify status buffer mappings
+		status = {
+			-- Adds a mapping with "B" as key that does the "BranchPopup" command
+			["B"] = "BranchPopup",
+			-- Removes the default mapping of "s"
+			["s"] = "",
+		}
+	}
+}
+
+require'rust-tools'.setup{
+}
 
