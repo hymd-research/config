8dee352a241d549b6f106de103b32f6e3f0dc5d6
hymd <hymd.email@gmail.com>
2022-06-25 05:32:52 +0900

    feat: add dependencies with brew

---
+++ fish/conf.d/cmake.fish
@@ -0,0 +1,3 @@
+set -x CMAKE_ASM_COMPILER /Library/Developer/CommandLineTools/usr/bin/cc
+set -x CMAKE_CXX_COMPILER /Library/Developer/CommandLineTools/usr/bin/c++
+set -x CMAKE_C_COMPILER /Library/Developer/CommandLineTools/usr/bin/cc
