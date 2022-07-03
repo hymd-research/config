1afc06f587de0232cb2ca31e296ddc30980055f9
hymd <hymd.email@gmail.com>
2022-07-04 01:07:17 +0900

    feat(fish): path for C headers and libs

---
+++ fish/conf.d/cpath.fish
@@ -0,0 +1,3 @@
+set -x LIBRARY_PATH /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib
+set -x C_INCLUDE_PATH /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include
+set -x CPLUS_INCLUDE_PATH /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include
--- fish/conf.d/cpp_include_paths.fish
+++
@@ -1,1 +0,0 @@
-set -gx CPPFLAGS "-I/usr/local/opt/openjdk@8/include"
